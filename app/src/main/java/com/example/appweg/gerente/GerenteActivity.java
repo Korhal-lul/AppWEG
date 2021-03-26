0a8dd7af972f8dfa01528c3ba3d4a57863705bec
SrKeppler <gabriel_da-costa@estudante.sc.senai.br>
2021-03-26 14:50:33 -0300

    Organização

---
+++ app/src/main/java/com/example/appweg/gerente/GerenteActivity.java
@@ -0,0 +1,16 @@
+package com.example.appweg.gerente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appweg.R;
+
+public class GerenteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerente);
+    }
+}
